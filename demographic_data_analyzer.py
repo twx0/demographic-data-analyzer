78aabac80b37fdf0f552ef225ec413bab742cab5
K. A. Astad <Kris@twxo.net>
2024-04-24 11:11:32 +0000

    added round

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -9,10 +9,10 @@ def calculate_demographic_data(print_data=True):
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = df[df['sex'] == 'Male']['age'].mean()
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = (df['education'] == 'Bachelors').mean() * 100
+    percentage_bachelors = round((df['education'] == 'Bachelors').mean() * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
